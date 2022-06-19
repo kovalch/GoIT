c7c69e10051cfed982314b29be353f0893234217
kovalch <nataliia.kovalchuk@liip.ch>
2022-06-19 13:16:37 +0200

    fix: Add explicit creation of the class exemplars

--- contact_book/contact_book_class.py
+++ contact_book/contact_book_class.py
@@ -28,32 +28,29 @@ class Record:
             print(f"and a phone number {self.phone}")
 
     def add_phone_number(self, phone_number):
-        new_phone = Phone(phone_number)
-        if new_phone not in self.phone:
-            print(f"Adding new phone number: {new_phone}")
-            self.phone.append(new_phone)
+        if phone_number not in self.phone:
+            print(f"Adding new phone number: {phone_number}")
+            self.phone.append(phone_number)
 
     def find_phone_number(self, phone_number):
         for i in self.phone:
             if i == phone_number:
                 print(f"{i} number was found")
                 return i
-            else:
-                print("Cannot find a given phone number")
+        print("Cannot find a given phone number")
 
     def delete_phone_number(self, phone_number):
         phone_number_to_delete = self.find_phone_number(phone_number)
-        if phone_number_to_delete in self.phone:
+        if phone_number_to_delete:
             print(f"{phone_number_to_delete} number are going to be deleted")
             self.phone.remove(phone_number_to_delete)
 
     def edit_phone_number(self, phone_number, new_phone_number):
         phone_number_to_edit = self.find_phone_number(phone_number)
-        new_phone = Phone(new_phone_number)
-        if phone_number_to_edit in self.phone:
+        if phone_number_to_edit:
             self.phone.remove(phone_number_to_edit)
-            self.phone.append(new_phone)
-            print(f"{phone_number_to_edit} number was changed to {new_phone}")
+            self.phone.append(new_phone_number)
+            print(f"{phone_number_to_edit} number was changed to {new_phone_number}")
 
 
 class AddressBook(UserDict):
@@ -65,11 +62,17 @@ class AddressBook(UserDict):
 
 name = Name("Bill")
 phone = Phone("12345")
+phone2 = Phone("343434")
+phone3 = Phone("555555")
+phone4 = Phone("12345")
 rec = Record(name, phone)
-rec.add_phone_number("343434")
-rec.find_phone_number("343434")
-rec.edit_phone_number("343434","555555")
-rec.add_phone_number("343434")
+rec.add_phone_number(phone2)
+print(rec.phone)
+rec.find_phone_number(phone2)
+rec.edit_phone_number(phone2,phone3)
+rec.add_phone_number(phone2)
+rec.add_phone_number(phone4)
+rec.delete_phone_number(phone)
 print(rec.phone)
 ab = AddressBook()
 ab.add_record(rec)
